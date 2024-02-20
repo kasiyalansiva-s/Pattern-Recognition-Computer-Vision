ad534ddf5697473e41a2664e9a83de919761202b
kasiyalansiva-s <156709412+kasiyalansiva-s@users.noreply.github.com>
2024-02-20 16:04:03 -0500

    Add files via upload

--- Project 3/task1/Task1.cpp
+++ Project 3/task1/Task1.cpp
@@ -23,6 +23,24 @@ static int dynmcThresholding(Mat& image) {
 }
 
 /// Function for Binary Inverse Thresholding ///
+static void normalThresholding(Mat& image, int thresholdvalue, Mat& result) {
+    Mat gray_img;
+    if (image.channels() == 3)
+        cvtColor(image, gray_img, cv::COLOR_BGR2GRAY);
+    else
+        gray_img = image.clone();
+
+    result = Mat(gray_img.rows, gray_img.cols, CV_8UC1);
+
+    for (int y = 0; y < gray_img.rows; ++y) {
+        for (int x = 0; x < gray_img.cols; ++x) {
+            if (gray_img.at<uchar>(y, x) >= thresholdvalue)
+                result.at<uchar>(y, x) = 255; 
+            else
+                result.at<uchar>(y, x) = 0; 
+        }
+    }
+}
 static void binaryInverseThresholding(Mat& image, int thresholdvalue, Mat& result) {
     Mat gray_img;
     if (image.channels() == 3)
@@ -35,44 +53,41 @@ static void binaryInverseThresholding(Mat& image, int thresholdvalue, Mat& resul
     for (int y = 0; y < gray_img.rows; ++y) {
         for (int x = 0; x < gray_img.cols; ++x) {
             if (gray_img.at<uchar>(y, x) >= thresholdvalue)
-                result.at<uchar>(y, x) = 0; 
+                result.at<uchar>(y, x) = 0;
             else
-                result.at<uchar>(y, x) = 255; 
+                result.at<uchar>(y, x) = 255;
         }
     }
 }
 
 int main() {
-    VideoCapture cap(0); 
-    if (!cap.isOpened()) { 
+    VideoCapture cap(0);
+    if (!cap.isOpened()) {
         cerr << "Error: Unable to open camera!" << endl;
         return -1;
     }
-
     Mat frame;
-    Mat thresholded;
-
+    Mat thresholded_normal;
+    Mat thresholded_binary_inverse;
     while (true) {
-        cap >> frame; 
+        cap >> frame;
         if (frame.empty())
             break;
 
         Mat gray;
         GaussianBlur(frame, frame, Size(5, 5), 0, 10);
         cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
+        int threshold_value = dynmcThresholding(frame);
+        normalThresholding(gray, threshold_value, thresholded_normal);
+        binaryInverseThresholding(gray, threshold_value, thresholded_binary_inverse);
+        Mat display;
+        hconcat(thresholded_normal, thresholded_binary_inverse, display);
+        imshow("Thresholded Images", display);
 
-        
-        int threshold_value = dynmcThresholding(frame); 
-        binaryInverseThresholding(gray, threshold_value, thresholded);
-
-        
-        imshow("Thresholded", thresholded);
-
-        if (waitKey(30) >= 0) // Press any key to exit
+        if (waitKey(30) >= 0)
             break;
     }
 
     cap.release();
-    
     return 0;
 }
